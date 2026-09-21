3b2b42edf95846fde54298b7d95caa79fa72237f
AzamAzis <aziz.76218@gmail.com>
2026-09-21 14:07:37 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const hanakoKun = {
+	title: "Toilet Bound Hanako Kun",
+	author: "AidaIro",
+	pages: 131,
+	read: "it's read",
+
+	info: function() {
+		return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+	},
+};
+
+console.log(hanakoKun.info());
